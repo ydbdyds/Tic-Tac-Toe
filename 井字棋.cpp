ea9dab7ed95c900910636919b99ca35297c059f9
ydbdyds <1004903113@qq.com>
2019-05-25 22:52:24 +0800

    第二次提交 增加电脑模式选择 完善胜负判定

--- 井字棋.cpp
+++ 井字棋.cpp
@@ -40,7 +40,7 @@ void showBackground()//绘制棋盘
 }
 
 static player p1, p2;
-int s[3][3], tf = 0, gofirst = 1, row, column, over;
+int s[3][3], tf = 0, gofirst = 1, row, column, over,mode;
 bool jieshu = false;
 IMAGE image_x, image_o;
 int Array_x1 = 0, Array_y1 = 0;
@@ -147,7 +147,6 @@ void computer(int mode)
 			else if (s[2][2] == 1 && s[1][1] == 2 && s[2][0] == 0) s[2][0] = 2;
 			else
 			{
-				//cout << "here!" << endl;
 				for (int x = 0; x < 3; x++)
 				{
 					for (int y = 0; y < 3; y++)
@@ -185,7 +184,6 @@ void computer(int mode)
 }
 void Initialize()//初始化棋盘背景
 {
-
 	initgraph(8 * Unitlength, 8 * Unitlength);
 	setbkcolor(WHITE);
 	cleardevice();
@@ -196,14 +194,15 @@ void Initialize()//初始化棋盘背景
 	showchesspieces();
 	EndBatchDraw();
 	char z[3];
+	char choicemode[3];
 	InputBox(z, 3, "选择游戏模式 电脑模式（0） 双人模式（1） 结束游戏（2）");
-
 	// 将用户输入转换为数字
-
 	sscanf(z, "%d", &over);
-
-
-
+	if (over == 0)
+		{
+		InputBox(choicemode, 3, "选择电脑难度 简单模式(0) 普通模式（1）");
+		sscanf(choicemode, "%d", &mode);
+	}
 }
 
 void User_relatedinput()//玩家动作
@@ -228,10 +227,9 @@ void User_relatedinput()//玩家动作
 		sprintf(a, _T("%05d"), Array_y1);//纵坐标
 		outtextxy(Unitlength * 5, Unitlength * 7.5, a);
 
-
 		if (over == 0)
 		{
-			int mode = 1;
+			 
 			if (tf == 0)
 			{
 				for (i = 0; i < 3; i++)
@@ -327,44 +325,33 @@ void User_relatedinput()//玩家动作
 }
 
 
-
-void print()
-{
-	cout << "-------" << endl;
-	for (int x = 0; x < 3; x++)
-	{
-		cout << "|";
-		for (int y = 0; y < 3; y++)
-		{
-			if (s[x][y] == 0) cout << " ";
-			else if (s[x][y] == 1) cout << "O";
-			else if (s[x][y] == 2) cout << "X";
-			cout << "|";
-		}
-		cout << endl << "-------" << endl;
-	}
-	cout << endl;
-}
-
 int check()
 {
-	char tellwin[] = "恭喜你赢了!";
+	char tellWin[] = "恭喜你赢了!";
+	char tellLoser[] = "很遗憾,你输了.";
+	char tellDraw[] = "打平！";
 	for (int x = 0; x < 3; x++)
 	{
 		if (s[x][0] == 1 && s[x][1] == 1 && s[x][2] == 1)
 		{
-			if (over == 0) { cout << "你赢了！！！" << endl << endl; }
+			if (over == 0) {
+				settextstyle(Unitlength, 0, _T("黑体"));
+				settextcolor(RED);
+				outtextxy(Unitlength, Unitlength * 5, tellWin);
+			}
 			if (over == 2) { cout << p1.getName() + "赢了！强！" << endl << endl; }
-			settextstyle(Unitlength, 0, _T("黑体"));
-			settextcolor(RED);
-			outtextxy(Unitlength, Unitlength * 5, tellwin);
+
 			jieshu = true;
 			return 0;
 			break;
 		}
 		if (s[x][0] == 2 && s[x][1] == 2 && s[x][2] == 2)
 		{
-			if (over == 0)cout << "你输了" << endl << endl;
+			if (over == 0) {
+				settextstyle(Unitlength, 0, _T("黑体"));
+				settextcolor(RED);
+				outtextxy(Unitlength, Unitlength * 5, tellLoser);
+			}
 			if (over == 2) { cout << p2.getName() + "赢了！强！" << endl << endl; }
 			jieshu = true;
 			return 0;
@@ -375,7 +362,11 @@ int check()
 	{
 		if (s[0][x] == 1 && s[1][x] == 1 && s[2][x] == 1)
 		{
-			if (over == 0) { cout << "你赢了！！！" << endl << endl; }
+			if (over == 0) {
+				settextstyle(Unitlength, 0, _T("黑体"));
+				settextcolor(RED);
+				outtextxy(Unitlength, Unitlength * 5, tellWin);
+			}
 			if (over == 2) { cout << p1.getName() + "赢了！强！" << endl << endl; }
 			jieshu = true;
 			return 0;
@@ -383,7 +374,11 @@ int check()
 		}
 		if (s[0][x] == 2 && s[1][x] == 2 && s[2][x] == 2)
 		{
-			if (over == 0) { cout << "你输了" << endl << endl; }
+			if (over == 0) {
+				settextstyle(Unitlength, 0, _T("黑体"));
+				settextcolor(RED);
+				outtextxy(Unitlength, Unitlength * 5, tellLoser);
+			}
 			if (over == 2) { cout << p2.getName() + "赢了！强！" << endl << endl; }
 			jieshu = true;
 			return 0;
@@ -394,14 +389,22 @@ int check()
 
 	if (s[0][0] == 1 && s[1][1] == 1 && s[2][2] == 1 || s[2][0] == 1 && s[1][1] == 1 && s[0][2] == 1)
 	{
-		if (over == 0) { cout << "你赢了!!!" << endl << endl; }
+		if (over == 0) {
+			settextstyle(Unitlength, 0, _T("黑体"));
+			settextcolor(RED);
+			outtextxy(Unitlength, Unitlength * 5, tellWin);
+		}
 		if (over == 2) { cout << p1.getName() + "赢了！强！" << endl << endl; }
 		jieshu = true;
 		return 0;
 	}
 	else if (s[0][0] == 2 && s[1][1] == 2 && s[2][2] == 2 || s[2][0] == 2 && s[1][1] == 2 && s[0][2] == 2)
 	{
-		if (over == 0) { cout << "你输了" << endl << endl; }
+		if (over == 0) {
+			settextstyle(Unitlength, 0, _T("黑体"));
+			settextcolor(RED);
+			outtextxy(Unitlength, Unitlength * 5, tellLoser);
+		}
 		if (over == 2) { cout << p2.getName() + "赢了！强！" << endl << endl; }
 		return 0;
 	}
@@ -418,7 +421,9 @@ int check()
 		}
 		if (count == 9)
 		{
-			cout << "平局" << endl << endl;
+			settextstyle(Unitlength, 0, _T("黑体"));
+			settextcolor(RED);
+			outtextxy(Unitlength, Unitlength * 5, tellDraw);
 			jieshu = true;
 			return 0;
 		}
@@ -437,53 +442,10 @@ int main()
 		User_relatedinput();
 		check();
 	}
-	cout << "小帅哥小美女快来玩啊" << endl;
 
 	cout << "电脑模式（0），结束游戏（1），双人模式（2）: ";
 	cin >> over;
-	while (over == 0)
-	{
-		cout << "你想选择什么难度" << endl << "简单模式（0） 普通模式（1） ";
-		int mode;
-		cin >> mode;
-		jieshu = false;
-		for (int x = 0; x < 3; x++)
-			for (int y = 0; y < 3; y++) s[x][y] = 0;
-		cout << "你想先走(1) 还是让电脑先走(0): ";
-		cin >> gofirst;
-		if (gofirst == 1) tf = 0;
-		else if (gofirst == 0) tf = 1;
-		print();
-		while (jieshu == false)
-		{
-			bool inputtf = true;
-			if (tf % 2 == 0)
-			{
-				while (inputtf)
-				{
-					cout << "你想下在哪一步?" << endl;
-					cout << "行: ";
-					cin >> row;
-					cout << "列: ";
-					cin >> column;
-					row--;
-					column--;
-					if (row < 0 || row >= 3 || column < 0 || column >= 3 || s[row][column] != 0)
-					{
-						cout << "啊，那里不行~" << endl;
-					}
-					else inputtf = false;
-				}
-				s[row][column] = 1;
-			}
-			else computer(mode);
-			print();
-			check();
-			tf++;
-		}
-		cout << "如果你想再玩一次请扣 0, 不想就扣 1: ";
-		cin >> over;
-	}
+	
 
 	while (over == 2)
 	{
@@ -509,7 +471,7 @@ int main()
 		cin >> gofirst;
 		if (gofirst == 1) tf = 0;
 		else if (gofirst == 0) tf = 1;
-		print();
+
 		while (jieshu == false)
 		{
 			bool inputtf = true;
@@ -548,7 +510,7 @@ int main()
 				else s[row][column] = 2;
 			};
 
-			print();
+
 			check();
 			tf++;
 		}
